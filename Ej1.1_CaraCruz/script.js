3fe165c0bb49a31d5bc817637f647434561d0f6c
ncocana <ncocana@cifpfbmoll.eu>
2023-10-11 17:51:54 +0200

    feat(ej1.1_CaraCruz): add images

--- Ej1.1_CaraCruz/script.js
+++ Ej1.1_CaraCruz/script.js
@@ -5,9 +5,22 @@ var output;
 function play() {
     output = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER)
 
+    var img = document.createElement("img");
+    var src = document.getElementById("image");
+
     if (output == 1) {
+        if (src.hasChildNodes()) {
+            src.removeChild(src.firstChild);
+        }
+        img.src = "./img/cara.jpg";
+        src.appendChild(img);
         document.getElementById("output").innerHTML = "Ha salido cara.<br>¡Has ganado!"
     } else if (output == 2) {
+        if (src.hasChildNodes()) {
+            src.removeChild(src.firstChild);
+        }
+        img.src = "./img/cruz.jpg";
+        src.appendChild(img);
         document.getElementById("output").innerHTML = "Ha salido cruz.<br>¡Has perdido!"
     }
 }
