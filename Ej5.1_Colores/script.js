0c61180b34840ec6b304dc8a64c6d08dbdce2b93
ncocana <ncocana@cifpfbmoll.eu>
2023-11-13 17:23:19 +0100

    feat(index, script): exercise colores

---
+++ Ej5.1_Colores/script.js
@@ -0,0 +1,25 @@
+function paintSquare() {
+    let heightMin = document.getElementById("heightMin").value;
+    let heightMax = document.getElementById("heightMax").value;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            // console.log(xhttp.responseText);
+            // console.log(JSON.parse(xhttp.responseText));
+            let response = JSON.parse(xhttp.responseText);
+            if (response.error) {
+                document.getElementById("outputDiv").innerHTML = "<p>" + response.error + "</p>";
+                document.getElementById("outputDiv").style = ""
+            } else {
+                document.getElementById("outputDiv").innerHTML = "";
+                document.getElementById("outputDiv").style.width = response.width + "px";
+                document.getElementById("outputDiv").style.height = response.height + "px";
+                document.getElementById("outputDiv").style.backgroundColor = response.background;
+                document.getElementById("outputDiv").style.borderColor = response.color;
+            }
+        }
+    };
+    xhttp.open("GET", "http://95.39.145.183/ejercicios/color.php?min=" + heightMin + "&max=" + heightMax, true);
+    xhttp.send();
+}
