121500b6c436e5054f43a543fb55989029f5ffbd
ncocana <ncocana@cifpfbmoll.eu>
2024-01-12 19:11:50 +0100

    feat(Ej6.3_Mapa_Espana): exercise finished

---
+++ Ej6.3_Mapa_Espana/main.js
@@ -0,0 +1,26 @@
+//aquí el código JS
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, targetArea) {
+    ev.preventDefault();
+
+    var data = ev.dataTransfer.getData("text");
+    var img = document.getElementById(data);
+    var area = document.getElementById(targetArea);
+
+    if (img.getAttribute("alt") == area.getAttribute("title")) {
+        img.style.position = 'absolute';
+        img.style.left = area.getAttribute("data-left");
+        img.style.top = area.getAttribute("data-top");
+
+        ev.target.appendChild(img);
+    } else {
+        alert("La provincia no encaja en esta area.");
+    }
+}
